3ff12f221428a7f7f9c16eb7c8b2cc462fe9e681
Jaja Harris <jajaharris@gmail.com>
2020-05-23 10:28:12 -0700

    completed Part 1 Module 6

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,5 +1,73 @@
 import React from 'react';
+import { server, Body } from '../../lib/api';
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables,
+} from './types';
 
-export const Listings = () => {
-  return <h2>TinyHouse Listings</h2>;
+const LISTINGS_QUERY = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING_QUERY = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({
+      query: LISTINGS_QUERY,
+    });
+    console.log(data.listings);
+  };
+
+  const deleteListing = async () => {
+    const deleteBody: Body<DeleteListingVariables> = {
+      query: DELETE_LISTING_QUERY,
+      variables: {
+        id: '5ec81b982bca25a266a96a8a',
+      },      
+    };
+
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >(deleteBody);
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>
+        <button onClick={fetchListings}>Query Listings!</button>
+      </div>
+
+      <div>
+        <button onClick={deleteListing}>Delete Listings!</button>
+      </div>
+    </div>
+  );
 };
